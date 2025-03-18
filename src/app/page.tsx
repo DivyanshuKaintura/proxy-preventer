a5032be863345932db2e5712c7df705637bad5b1
Divyanshu Kaintura <divyanshukaintura789@gmail.com>
2025-03-18 15:02:11 +0530

    updatee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import StudentPage from "./student/page";
-import TeacherPage from "./teacher/page";
 import TeacherOrStudentPage from "@/components/home/teacher-or-student/page";
 
 export default function Home() {
